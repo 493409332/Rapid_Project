14cd42e3ecc4ed0965c26642510e6c90d2c49505
mianitao <493409332@qq.com>
2015-01-19 16:43:27 +0800

    初始化推送！

---
+++ Complex/Logical/AopAttribute/StartAttribute.cs
@@ -0,0 +1,85 @@
+﻿using MtAop.BaseAttribute;
+using MtAop.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Complex.Entity;
+namespace Complex.Logical.AopAttribute
+{
+    public class StartAttribute : PreAspectAttribute
+    {
+        public override InvokeContext Action(InvokeContext context)
+        {
+            for ( int ii = 0; ii < context.Parameters.Count;ii++ )
+            {
+                //if ( item is test2 )
+                //{
+                //    test2 aaa = (test2) item;
+                //    if ( aaa.ID == 1 )
+                //    {
+                //        aaa.ID = 100;
+                //    }
+
+                //}
+                if ( context.Parameters[ii] is test2 )
+                {
+                    test2 aaa=new test2();
+
+                    aaa = context.Parameters[ii] as test2;
+                    if ( aaa.ID == 1 )
+                    {
+                        aaa.ID = 100;
+                    }
+                }
+                if ( context.Parameters[ii] is int )
+                { 
+                    context.Parameters[ii] = 1111;
+                }
+
+            }
+
+            Console.WriteLine("log start!");
+           context.IsRun = true;
+            return context;
+        }
+    }
+    public class Start1Attribute : PreAspectAttribute
+    {
+        public override InvokeContext Action(InvokeContext context)
+        {
+            for ( int ii = 0; ii < context.Parameters.Count; ii++ )
+            {
+                //if ( item is test2 )
+                //{
+                //    test2 aaa = (test2) item;
+                //    if ( aaa.ID == 1 )
+                //    {
+                //        aaa.ID = 100;
+                //    }
+
+                //}
+                if ( context.Parameters[ii] is test2 )
+                {
+                    test2 aaa = new test2();
+
+                    aaa = context.Parameters[ii] as test2;
+                    if ( aaa.ID == 1 )
+                    {
+                        aaa.ID = 100;
+                    }
+                }
+                if ( context.Parameters[ii] is int )
+                {
+                    context.Parameters[ii] = 1111;
+                }
+
+            }
+
+            Console.WriteLine("log start!");
+            context.IsRun = true;
+            return context;
+        }
+    }
+}
